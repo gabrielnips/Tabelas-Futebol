190c40af79dd4561f29e5174bacdc819892606e7
Gabriel <hzfivem@gmail.com>
2023-11-03 07:21:30 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const API_KEY = 'test_067c14e74bae57183ad395';
+const API_KEY = 'test_a7e9f3017b1910fe2d03e391d6d20a';
 // pegue a apikey em https://api-futebol.com.br/dashboard
 export default API_KEY;
